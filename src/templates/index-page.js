adea73fdb6584c4d166864bc7280b23ed560790b
Corey Bruyere <coreybruyere@ymail.com>
2019-04-24 22:27:18 -0700

    fixxy

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -69,12 +69,12 @@ export const IndexPageTemplate = ({
             <div className="column is-10 is-offset-1">
               <div className="content">
                 <div className="content">
-                  <div className="tile">
+                  {/* <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
                     <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="columns">
                   <div className="column is-12" id="about">
